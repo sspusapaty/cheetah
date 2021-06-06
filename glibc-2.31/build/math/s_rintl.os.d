ce3b4ff5cf20ef5b1ab442c8896ef0798e5b7437
Sameer Pusapaty <ubuntu@ip-172-31-40-112.ec2.internal>
2021-06-06 15:43:57 +0000

    i think i linked my own glibc

---
+++ glibc-2.31/build/math/s_rintl.os.d
@@ -0,0 +1,41 @@
+$(common-objpfx)math/s_rintl.os: \
+ ../sysdeps/x86_64/fpu/s_rintl.c ../include/stdc-predef.h \
+ $(common-objpfx)libc-modules.h \
+ ../include/libc-symbols.h \
+ $(common-objpfx)config.h \
+ ../sysdeps/generic/symbol-hacks.h ../sysdeps/i386/fpu/s_rintl.c \
+ ../sysdeps/generic/libm-alias-ldouble.h ../sysdeps/x86/bits/floatn.h \
+ ../include/features.h ../include/sys/cdefs.h ../misc/sys/cdefs.h \
+ ../sysdeps/x86/bits/wordsize.h \
+ ../sysdeps/ieee754/ldbl-96/bits/long-double.h ../include/gnu/stubs.h \
+ ../bits/floatn-common.h
+
+../include/stdc-predef.h:
+
+$(common-objpfx)libc-modules.h:
+
+../include/libc-symbols.h:
+
+$(common-objpfx)config.h:
+
+../sysdeps/generic/symbol-hacks.h:
+
+../sysdeps/i386/fpu/s_rintl.c:
+
+../sysdeps/generic/libm-alias-ldouble.h:
+
+../sysdeps/x86/bits/floatn.h:
+
+../include/features.h:
+
+../include/sys/cdefs.h:
+
+../misc/sys/cdefs.h:
+
+../sysdeps/x86/bits/wordsize.h:
+
+../sysdeps/ieee754/ldbl-96/bits/long-double.h:
+
+../include/gnu/stubs.h:
+
+../bits/floatn-common.h:
---
+++ glibc-2.31/build/runtime-linker.h
@@ -0,0 +1,1 @@
+#define RUNTIME_LINKER "/home/ubuntu/opencilk-project/cheetah/glibc-2.31/build/install/lib/ld-linux-x86-64.so.2"
