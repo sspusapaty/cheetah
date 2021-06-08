8eb7a3885f783b335282f2eb3ec71a5b0d69ab69
Sameer Pusapaty <ubuntu@ip-172-31-40-112.ec2.internal>
2021-06-08 21:27:43 +0000

    cleaned up code

--- glibc-2.31/build/elf/trusted-dirs.h
+++
@@ -1,8 +0,0 @@
-#define SYSTEM_DIRS \
-  "/home/ubuntu/opencilk-project/cheetah/glibc-2.31/build/install/lib/"
-
-#define SYSTEM_DIRS_LEN \
-  67
-
-#define SYSTEM_DIRS_MAX_LEN	67
-#define DL_DST_LIB "lib"
--- glibc-2.31/build/runtime-linker.h
+++
@@ -1,1 +0,0 @@
-#define RUNTIME_LINKER "/home/ubuntu/opencilk-project/cheetah/glibc-2.31/build/install/lib/ld-linux-x86-64.so.2"
